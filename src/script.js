7f0b0c3b4d5260d22c00911e90e242f22f8ebaa0
doxas <eizanco@gmail.com>
2019-02-18 18:27:40 +0900

    fix script

--- src/script.js
+++ src/script.js
@@ -1,4 +1,9 @@
 
-(() => {
-})();
+import Root from './components/index.js';
+
+window.addEventListener('load', () => {
+    let c = new Root.Base.ComponentSample();
+    document.body.appendChild(c.getDOM());
+}, false);
+
 
