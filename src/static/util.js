d16a3de12c0b8f2d21833ed90b0a74dcc8954d8e
doxas <eizanco@gmail.com>
2019-03-04 12:27:33 +0900

    update error util

--- src/static/util.js
+++ src/static/util.js
@@ -4,9 +4,7 @@ import CONST from './constant.js';
 export default class Util {
     static get Math(){return MathUtil;}
     static get Str(){return StringUtil;}
-    static get Error(msg, name = ''){
-        return ErrorUtil.err(msg, name);
-    }
+    static get Error(){return ErrorUtil;}
     static isNumber(n){
         return n != null && Object.prototype.toString.call(n) === '[object Number]';
     }
@@ -89,13 +87,25 @@ class StringUtil {
 }
 
 class ErrorUtil {
-    static err(msg, name = ''){
+    static new(msg, name = '', type = ''){
         let method = '';
         if(Util.isString(name) === true && name !== ''){
             method = `: ${name}`;
         }
         let message = `${CONST.GLOBAL_PREFIX}[${CONST.GLOBAL_NAME}${method}] ${msg}`;
-        return new Error(msg);
+        switch(type){
+            case 'reference':
+                return new ReferenceError(msg);
+            case 'syntax':
+                return new SyntaxError(msg);
+            case 'type':
+                return new TypeError(msg);
+            default:
+                return new Error(msg);
+        }
+    }
+    static throw(msg, name = '', type = ''){
+        throw ErrorUtil.new(msg, name, type);
     }
 }
 
