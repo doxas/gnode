be2cab3dcca79644741a73151e7f879e20220f8f
doxas <eizanco@gmail.com>
2019-03-04 12:45:09 +0900

    small fix prefix

--- src/static/constant.js
+++ src/static/constant.js
@@ -1,7 +1,7 @@
 
 export default {
     GLOBAL_NAME: 'gnode',
-    GLOBAL_PREFIX: '🔗 ',
+    GLOBAL_PREFIX: '😲',
     COMPONENT_DEFAULT_BACKGROUND_COLOR: 'whitesmoke',
     COMPONENT_DEFAULT_BACKGROUND_HOVER_COLOR: 'white',
     COMPONENT_DEFAULT_COLOR: 'silver',
